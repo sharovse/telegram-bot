dcd3c1d436c2b3ec47555e7cb2dd8e2ec15660d2
Sharov Sergey <sharovse@inbox.ru>
2019-10-22 23:19:32 +0300

    add var

--- src/main/java/ru/sharovse/telegrambot/TelegramBotApplication.java
+++ src/main/java/ru/sharovse/telegrambot/TelegramBotApplication.java
@@ -1,11 +1,15 @@
 package ru.sharovse.telegrambot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
+	@Value("${var}")
+	String var;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
